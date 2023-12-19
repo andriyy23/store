8b84a7ccb25a843b05e307cfef7f3d60f95c8db0
andriyy23 <145467885+andriyy23@users.noreply.github.com>
2023-12-20 00:26:45 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React, { useState } from 'react'
+import Home from "./pages/Home.jsx"
+import { Routes, Route } from "react-router-dom"
+import Cart from './pages/Cart.jsx'
+import Product from './pages/Product.jsx'
 
+const App = () => {
+
+  const [choose, setChoose] = useState([])
+    
+  return (
+    <div>
+        <Routes>
+          <Route path="/" element={ <Home choose={choose} setChoose={setChoose} /> } />
+          <Route path={`/product/:productId`} element={ <Product />} />
+          <Route path="/cart" element={ <Cart choose={choose} setChoose={setChoose} /> } />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
